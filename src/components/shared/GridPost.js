8f559d915cb01044c78f21443e50a77f83d147bc
Senadin <beganovicsenadin24@gmail.com>
2020-07-05 23:12:52 +0200

    started working  on explore page, addded suggestions and started working on gird post.

--- src/components/shared/GridPost.js
+++ src/components/shared/GridPost.js
@@ -1,10 +1,20 @@
 import React from "react";
 import { useGridPostStyles } from "../../styles";
+import { Typography } from "@material-ui/core";
 
-function GridPost() {
-  useGridPostStyles();
+function GridPost({ post }) {
+  const classes = useGridPostStyles();
 
-  return <div>GridPost</div>;
+  return (
+    <div className={classes.gridPostContainer}>
+      <div className={classes.gridPostOverlay}>
+        <div className={classes.gridPostInfo}>
+          <span className={classes.likes}/>
+          <Typography >Hello</Typography>
+        </div>
+      </div> 
+    </div>
+  )
 }
 
 export default GridPost;
