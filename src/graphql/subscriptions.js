557906ae95be61bfd49fb0ef9a11a96a7c378228
Senadin <beganovicsenadin24@gmail.com>
2020-07-10 14:17:24 +0200

    refactored code a bit to resolve error

--- src/graphql/subscriptions.js
+++ src/graphql/subscriptions.js
@@ -1,12 +1,12 @@
-import gql from "apollo-boost";
+import { gql } from "apollo-boost";
 
 export const ME = gql`
-  subscription me($userId: String!) {
+  subscription me($userId: String) {
     users(where: { user_id: { _eq: $userId } }) {
       id
       user_id
-      username
       name
+      username
       profile_image
       last_checked
     }
